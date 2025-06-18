17eef15b57b0295c77ed29a097a2fb13f997c2a6
Xuezhi He <hexu1011@h-ka.de>
2025-06-18 08:58:47 +0200

    Add more basic tests for frontendbuch

--- tests/frontendbuch.test.ts
+++ tests/frontendbuch.test.ts
@@ -1,6 +1,5 @@
 import { test, expect } from '@playwright/test';
 import { StartseitePage } from './pages/StartseitePage';
-import { SuchPage } from './pages/SuchPage';
 
 test('has title', async ({ page }) => {
   await page.goto('http://localhost:3001/');
@@ -17,5 +16,27 @@ test('go to suche page', async ({ page }) => {
   await startseite.clickSuche();
 
   // Expects page to have a heading with the name of Buch Details.
-  await expect(page.getByRole('heading', { name: 'Buch Details' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: /Buch Details/ })).toBeVisible({});
+});
+
+test('go to neu page', async ({ page }) => {
+  const startseite = new StartseitePage(page);
+
+  await page.goto('http://localhost:3001/');
+
+  await startseite.clickNeu();
+
+  // Expects page to have a heading with the name of Neues Buch anlegen.
+  await expect(page.getByRole('heading', { name: /Neues Buch anlegen/ })).toBeVisible({});
+});
+
+test('go to login page', async ({ page }) => {
+  const startseite = new StartseitePage(page);
+
+  await page.goto('http://localhost:3001/');
+
+  await startseite.clickLogin();
+
+  // Expects page to have a heading with the name of Einloggen.
+  await expect(page.getByRole('heading', { name: /Einloggen/ })).toBeVisible({});
 });
