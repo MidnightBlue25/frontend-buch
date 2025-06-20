2e31b42e1f077921b44789a21edaafa3ff8e7bce
Engin Dagdas <daen1012@h-ka.de>
2025-06-20 16:57:22 +0200

    Add links to sidebar

--- src/components/offcanvas.tsx
+++ src/components/offcanvas.tsx
@@ -4,6 +4,9 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import { RxHamburgerMenu } from 'react-icons/rx';
+import { FaGithub } from "react-icons/fa";
+import { SiReactbootstrap } from "react-icons/si";
+import { SiNextdotjs } from "react-icons/si";
 import IsbnGenerator from './isbnGenerator';
 
 export default function SimpleOffcanvas() {
@@ -22,10 +25,39 @@ export default function SimpleOffcanvas() {
           <Offcanvas.Title>Sidebar</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
-          {/* <h5>Tools</h5>  {/* → Überschrift */}
           <IsbnGenerator />
-          <br />
-          <p>&quot;Hier werden noch Sachen folgen.&quot; – Developer</p>
+          <hr />
+          <div className="mt-2">
+            <a
+              href="https://github.com/MidnightBlue25/frontend-buch"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="d-flex align-items-center gap-2 mb-2"
+            >
+              <FaGithub size={24} />
+              Zum GitHub-Projekt
+            </a>
+            <hr />
+            <p className="fw-semibold mb-2">Projekt erstellt mit:</p>
+            <a
+              href="https://nextjs.org/docs/app/getting-started/installation"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="d-flex align-items-center gap-2 mb-2"
+            >
+              <SiNextdotjs size={24} />
+              Next.js
+            </a>
+            <a
+              href="https://react-bootstrap.netlify.app/docs/getting-started/introduction"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="d-flex align-items-center gap-2"
+            >
+              <SiReactbootstrap size={24} />
+              React Bootstrap
+            </a>
+          </div>
         </Offcanvas.Body>
       </Offcanvas>
     </>
